4c535f1a195631f215c0b8555c8bca2827a4eb4c
Pontus <pontus@minabastapolare.se>
2021-04-24 15:38:51 +0200

    Started on pagan ball and some level stuff

--- Assets/Source/Enemys/PaeganBall.cs
+++ Assets/Source/Enemys/PaeganBall.cs
@@ -1,7 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PaeganBall : MonoBehaviour, Enemy {
+public class PaeganBall : MonoBehaviour, IEnemy {
 
-	
+	public int Health { get; set; }
+
+	private float ySpeed = 1f;
+	private float spinSpeed = 0.03f;
+	private float xTransitionTime = 3.5f;
+
+	private void Start()
+	{
+		spinSpeed = Random.Range(-0.03f, 0.03f);
+		StartCoroutine(LerpXPosition(Random.Range(-6.5f, 6.5f), xTransitionTime));
+	}
+
+	private void Update()
+	{
+		//float step = xSpeed * Time.deltaTime; // calculate distance to move
+		var step = ySpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, 6.5f, 0), step);
+		var spinStep = spinSpeed * Time.deltaTime;
+		transform.Rotate(Vector3.forward * spinSpeed);
+
+		if (transform.position.y > 6f) {
+			Destroy(gameObject);
+		}
+	}
+
+	public void Spawn(Vector3 pos)
+	{
+		transform.position = pos;
+	}
+
+	public void TakeDamage(int damage)
+	{
+		Health = -damage;
+		if (Health <= 0)
+		{
+			// Do the death things
+		}
+	}
+
+	IEnumerator LerpXPosition(float targetXPosition, float duration)
+	{
+		var time = 0f;
+		var startTime = Time.time;
+		var startPosition = transform.position.x;
+
+		while (time < duration)
+		{
+			var t = (Time.time - startTime) / duration;
+			transform.position = new Vector3(Mathf.SmoothStep(startPosition, targetXPosition, t), transform.position.y, transform.position.z);
+			time += Time.deltaTime;
+			yield return null;
+		}
+		StartCoroutine(LerpXPosition(Random.Range(-6.5f, 6.5f), xTransitionTime));
+
+	}
+
 }
--- Assets/Source/Level.cs
+++ Assets/Source/Level.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-	public GameObject enemy;
+	public GameObject[] enemies;
+	private Transform enemyContainer;
+	private float spawnCooldown = 5f;
+	private float spawnCounter = 0f;
 
-	public void Spawn() {
+	private void Start()
+	{
+		enemyContainer = LevelManager.Instance.transform.Find("EnemyContainer");
+	}
+
+	public void Tick()
+	{
+		spawnCounter += Time.deltaTime;
+		if (spawnCounter >= spawnCooldown) {
+			spawnCounter = 0f;
+			Spawn();
+		}
+	}
+
+	private void Spawn() {
+		var enemy = enemies[Random.Range(0, enemies.Length)];
 		var spawn = Instantiate(enemy, transform);
-
-		spawn.transform.position = new Vector3(0, 0, 0);
+		var ienemy = spawn.GetComponent<IEnemy>();
+		ienemy.Spawn(new Vector3(Random.Range(-6.5f, 6.5f), -5f));
+		spawn.transform.SetParent(enemyContainer);
 	}
 }
